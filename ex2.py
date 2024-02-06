da33fdf821f25d771b84408606830040333cd126
edu-porto <eduardo.porto@sou.inteli.edu.br>
2024-02-06 16:01:08 -0300

    ex ambos

--- ex2.py
+++ ex2.py
@@ -1,7 +1,9 @@
 from ex1 import sum_numbers 
 
 num1 = 392
-num2 = 142
+num2 = 144
 result = sum_numbers(num1,num2)
 
-print(f"somar {num1} com {num2}  = {result}")
+
+
+print(f"{num1} + {num2}  = {result}")
